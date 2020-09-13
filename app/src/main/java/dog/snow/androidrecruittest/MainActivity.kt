fa2e4f910fdd0e59eb2cbf4c77a297fde1b80a71
RolandMcDoland <miki.franek@gmail.com>
2020-09-13 20:00:32 +0200

    No internet mode

--- app/src/main/java/dog/snow/androidrecruittest/MainActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/MainActivity.kt
@@ -1,12 +1,24 @@
 package dog.snow.androidrecruittest
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import dog.snow.androidrecruittest.network.NetworkConnection
+import kotlinx.android.synthetic.main.layout_banner.*
 
 class MainActivity : AppCompatActivity(R.layout.main_activity){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val networkConnection = NetworkConnection(applicationContext)
+        networkConnection.observe(this, Observer { isConnected ->
+            if(isConnected)
+                banner.visibility = View.GONE
+            else
+                banner.visibility = View.VISIBLE
+        })
     }
 
 
